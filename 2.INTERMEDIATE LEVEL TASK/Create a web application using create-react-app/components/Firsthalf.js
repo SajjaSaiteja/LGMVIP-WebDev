03e51ad3c57248ddd676d430bf341ff1c8554283
SajjaSaiteja <58513324+SajjaSaiteja@users.noreply.github.com>
2021-10-23 12:23:29 +0530

    Add files via upload

---
+++ 2.INTERMEDIATE LEVEL TASK/Create a web application using create-react-app/components/Firsthalf.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../components/firsthalf.css'
+
+function Firsthalf({getusers}) {
+    return (
+        <div className="bg" id="bg">
+        <div className="navbar">
+            <div className="logo"><span>Elliptical</span></div>
+            <div className="ul">
+                <li><a href="#parent" onClick={()=>getusers()}>Users</a></li>
+            </div>
+        </div> 
+    </div>
+    )
+}
+
+export default Firsthalf
